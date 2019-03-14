1c41d5916361dd7cf3249844aba3e6cabb57724c
hubhack <2329259856@qq.com>
2019-03-14 17:28:11 +0800

    add

--- .vscode/study2.py
+++ .vscode/study2.py
@@ -4,7 +4,12 @@ print("你输入的内容:",str)
 for x in range (1,11):
     print(str(x), repr(x*x),)
     print('l')
-'''
+
 print('{name}网址： {site}'.format(name='菜鸟教程', site='www.runoob.com'))
 print('{0}网址:  {1}'.format('菜鸟教程','www.runoob.com'))
 print ('')
+'''
+#xpath
+#取属性名 @代表去属性
+#//标签名[@属性='属性值']
+#//div[@class = 'tool']
