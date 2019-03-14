f4d4e186bb6b07970d058a80c921691c9565b3b2
hubhack <2329259856@qq.com>
2019-03-14 11:12:26 +0800

    ..........

---
+++ .vscode/study2.py
@@ -0,0 +1,9 @@
+'''str  = input("你输入的是:")
+print("你输入的内容:",str)
+
+for x in range (1,11):
+    print(str(x), repr(x*x),)
+    print('l')
+'''
+print('{name}网址： {site}'.format(name='菜鸟教程', site='www.runoob.com'))
+print('{0}网址:  {1}'.format('菜鸟教程','www.runoob.com'))
--- study.py
+++ study.py
@@ -1,7 +1,1 @@
-class myclass:
-    i = 123456
-    def f(self):
-        return 'hello world'
-x = myclass()
-print("myclass 类的属性 i为:",x.i)
-print("mycalss 类的方法 f输出为:", x.f())
+2+3
