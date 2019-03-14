f3012669d9edb498540ce3b1ae3bc9390596bf93
hubhack <2329259856@qq.com>
2019-03-14 12:57:57 +0800

    提交

---
+++ except_error.py
@@ -0,0 +1,24 @@
+
+'''try:
+    fh = open ("testfile","w")
+    fh.write("处理异常文件")
+except IOError:
+    print "Error:没有找到文件或读取文件失败"
+else:
+    print"内容写入文件成功"
+    fh.close()
+
+while True:
+
+    try:
+        x = int(input("Please enter a number: "))
+        break
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
+'''
+try:
+    pass
+except expression as identifier:
+    pass
+else:
+    pass
